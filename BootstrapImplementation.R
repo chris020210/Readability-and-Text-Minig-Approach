f508ebfbc8fd3364258f14c1117e4683cfc30b4d
chris020210 <111938097+chris020210@users.noreply.github.com>
2023-04-28 14:46:37 -0600

    Add files via upload

--- BootstrapImplementation.R
+++ BootstrapImplementation.R
@@ -70,7 +70,7 @@ diffmatrix <- function(x,max.diff = 1,max.lag = 1){
 n <- length(myVariable)
 trend <- 1:n
 breaks <- 2
-model <- "break"
+model <- "crash"
 myVariable.diff <- diffmatrix(myVariable, max.diff = 1, max.lag = 1)
 # Run the test once to determine the values for the break dates
 # Definition of all the options needed
@@ -127,7 +127,7 @@ myZcoef <- na.omit(coef(lm(myVariable.diff ~ Z.diff)))
 
 # delta_1 from equation (7) of Chou 2007, which is needed for the manual calculation of S.tilde according to Chou 2007
 # equivalent results to the calculation with the cumulated sum
-delta_1 <- myVariable[1] - (Z[1,] %*% myZcoef)
+delta_1 <- as.numeric(myVariable[1] - (Z[1,] %*% myZcoef))
 
 #Result is equivalent to the original S.tilde
 myS.tilde <- myVariable - delta_1 - Z %*% myZcoef
@@ -145,7 +145,7 @@ boot.y <- foreach(i=1:n.sim, .combine = rbind, .packages = 'foreach') %dopar%{
   y.star <- delta_1 + as.vector((Z %*% myZcoef)) + sample(myS.tilde, size = length(myS.tilde), replace = TRUE)
   #y.star.matrix[,i] <- y.star
   #plotl(y.star)
-  test.stat <- ur.ls.bootstrap(y=y.star , model = model, breaks = breaks , method = "GTOS",pn = 0.1, critval = "bootstrap", print.results = "silent")
+  test.stat <- ur.ls.bootstrap(y=y.star , model = model, breaks = breaks , lags = 10, method = "Fixed",pn = 0.1, critval = "bootstrap", print.results = "silent")
   if(breaks == 2){
   return(list(c(unlist(test.stat[[1]]$`t-stat`)), c(unlist(test.stat[[1]]$`First break`)) ,c(unlist(test.stat[[1]]$`Second break`)),  c(as.numeric(test.stat[[1]]$Runtime, units = "mins"))))
   } else if (breaks == 1){
@@ -170,8 +170,8 @@ quantile(unlist(boot.y[,"Second break"]), c(.01, .025 ,.05, .1, .25, .5, .75, .9
 hist(unlist(boot.y[,"Second break"]), freq = F)
 hist(unlist(boot.y[,"First break"]))
 
-IQR_BOOTS_RESULTS <- test.results.y
-IQR_BOOTS_INTERVALS <- boot.y
+IQR_BOOTS_RESULTS_crash <- test.results.y
+IQR_BOOTS_INTERVALS_crash <- boot.y
 
 
 
@@ -185,7 +185,7 @@ cl <- makeCluster(max(1, detectCores() - 2))
 registerDoSNOW(cl)
 #Define variable y, which is the variable to be analysed
 
-myVariable <- na.omit(as.numeric(MPD_MAFSZ))
+myVariable <- as.vector(as.numeric(na.omit(MPD_MAFSZ)))
 
 lagmatrix <- function(x,max.lag){
   embed(c(rep(NA,max.lag),x),max.lag+1)
@@ -219,6 +219,158 @@ n <- length(myVariable)
 trend <- 1:n
 breaks <- 2
 model <- "break"
+myVariable.diff <- diffmatrix(myVariable, max.diff = 1, max.lag = 1)
+# Run the test once to determine the values for the break dates
+# Definition of all the options needed
+
+test.results.y <-  ur.ls.bootstrap(y=myVariable , model = model, breaks = breaks, lags = 11, method = "Fixed",pn = 0.1, critval = "bootstrap", print.results = "print")
+
+
+# Define variable Z, which is either Dt or DTt, depending on the model used
+# Create Dt and DTt, depending on the breaks, found in the initial application of the LS test
+
+
+#Define break dates
+myBreak1 <- test.results.y[[1]]$`First break`
+myBreak2 <- test.results.y[[1]]$`Second break`
+
+
+Dt1 <-  as.matrix(cbind(trend, trend >= (myBreak1 + 1)))
+
+#       Dummy with break in intercept and in trend
+
+DTt1 <- as.matrix(cbind(Dt1, c(rep(0, myBreak1), 1:(n - myBreak1))))
+colnames(Dt1) <- c("Trend","D")
+colnames(DTt1) <- c("Trend","D","DTt")
+Dt <- cbind(Dt1)
+DTt <- cbind(DTt1)
+
+
+Dt2 <-  as.matrix(trend >= (myBreak2 + 1))
+DTt2 <- as.matrix(cbind(Dt2, c(rep(0, myBreak2), 1:(n - myBreak2))))
+colnames(Dt2) <- c("D2")
+colnames(DTt2) <- c("D2","DTt2")
+#print(paste("Break2: ",myBreak2, sep = ""))
+
+#Combine all Dummies into one big matrix to make it easier to include in the regressions
+if(breaks == 1)
+{
+  Dt <- cbind(Dt1)
+  DTt <- cbind(DTt1)
+} else if(breaks == 2){
+  
+  Dt <- cbind(Dt1, Dt2)
+  DTt <- cbind(DTt1, DTt2)
+}
+
+if(model == "crash"){
+  Z <- Dt
+} else if(model == "break"){
+  
+  Z <- DTt
+}
+
+Z.diff <- diffmatrix(Z, max.diff = 1, max.lag = 1)
+#Matrix of coefficients, which omits the NA values
+myZcoef <- na.omit(coef(lm(myVariable.diff ~ Z.diff)))
+
+# delta_1 from equation (7) of Chou 2007, which is needed for the manual calculation of S.tilde according to Chou 2007
+# equivalent results to the calculation with the cumulated sum
+delta_1 <- as.numeric(myVariable[1] - (Z[1,] %*% myZcoef))
+
+#Result is equivalent to the original S.tilde
+myS.tilde <- myVariable - delta_1 - Z %*% myZcoef
+if(model == "crash"){
+  y.star <- delta_1 + as.vector((Dt %*% myZcoef)) + sample(myS.tilde, size = length(myS.tilde), replace = TRUE)
+} else if(model == "break"){
+  y.star <- delta_1 + as.vector((DTt %*% myZcoef)) + sample(myS.tilde, size = length(myS.tilde), replace = TRUE)
+  
+}
+
+
+#Loop for the actual boot strapping procedure
+
+boot.y <- foreach(i=1:n.sim, .combine = rbind, .packages = 'foreach') %dopar%{
+  y.star <- delta_1 + as.vector((Z %*% myZcoef)) + sample(myS.tilde, size = length(myS.tilde), replace = TRUE)
+  #y.star.matrix[,i] <- y.star
+  #plotl(y.star)
+  test.stat <- ur.ls.bootstrap(y=y.star , model = model, breaks = breaks, lag = 11, method = "Fixed",pn = 0.1, critval = "bootstrap", print.results = "silent")
+  if(breaks == 2){
+    return(list(c(unlist(test.stat[[1]]$`t-stat`)), c(unlist(test.stat[[1]]$`First break`)) ,c(unlist(test.stat[[1]]$`Second break`)),  c(as.numeric(test.stat[[1]]$Runtime, units = "mins"))))
+  } else if (breaks == 1){
+    return(c(c(unlist(test.stat[[1]]$`t-stat`)), c(unlist(test.stat[[1]]$`First break`)) , c(as.numeric(test.stat[[1]]$Runtime, units = "mins"))))
+  }
+}
+
+
+if(breaks == 2){
+  colnames(boot.y) <- c("t-stat","First break", "Second break", "Runtime")
+} else if (breaks == 1){
+  colnames(boot.y) <- c("t-stat","First break",  "Runtime")
+}
+
+
+
+MPD_BOOTS_RESULTS <- test.results.y
+MPD_BOOTS_INTERVALS <- boot.y
+#Calculate the critical values, based on the distribution of the test statistic
+hist(unlist(MPD_BOOTS_INTERVALS[,"t-stat"]), freq = FALSE)
+quantile(unlist(MPD_BOOTS_INTERVALS[,"t-stat"]), c(.01, .025 ,.05, .1, .25, .5, .75, .95, .975,.99))
+quantile(unlist(MPD_BOOTS_INTERVALS[,"First break"]), c(.01, .025 ,.05, .1, .25, .5, .75, .95, .975,.99))
+quantile(unlist(MPD_BOOTS_INTERVALS[,"Second break"]), c(.01, .025 ,.05, .1, .25, .5, .75, .95, .975,.99))
+
+hist(unlist(MPD_BOOTS_INTERVALS[,"Second break"]), freq = F)
+hist(unlist(MPD_BOOTS_INTERVALS[,"First break"]), freq = F)
+
+
+
+
+
+
+
+#######################################
+# MPMs -----------------------------------
+######################################
+
+cl <- makeCluster(max(1, detectCores() - 2))
+
+registerDoSNOW(cl)
+#Define variable y, which is the variable to be analysed
+
+myVariable <- as.vector(as.numeric(na.omit(MPM_MAFSZ)))
+
+lagmatrix <- function(x,max.lag){
+  embed(c(rep(NA,max.lag),x),max.lag+1)
+}
+#Add diffmatrix function
+diffmatrix <- function(x,max.diff = 1,max.lag = 1){
+  #Add if condition to make it possible to differentiate between matrix and vector                  
+  if(is.vector(x) == TRUE ){
+    myx <- embed(c(rep(NA,max.lag),diff(x,max.lag,max.diff)),max.diff)
+    colnames(myx) <- paste("v1.d",max.diff, sep=".")
+    return(myx)
+  }
+  
+  else if(is.matrix(x) == TRUE){
+    myx <- rbind(matrix(rep(NA,max.lag), ncol = ncol(x)), matrix(diff(x,max.lag,max.diff), ncol = ncol(x)))
+    mycolnames <- colnames(x)
+    colnames(myx) <- paste(mycolnames,"d",max.diff, sep=".")
+    return(myx)
+  }
+  #if matrix the result should be 0, if only a vector it should be 1
+  else if(as.integer(is.null(ncol(x))) == 0 ){
+    myx <- rbind(matrix(rep(NA,max.lag), ncol = ncol(x)), matrix(diff(x,max.lag,max.diff), ncol = ncol(x)))
+    mycolnames <- colnames(x)
+    colnames(myx) <- paste(mycolnames,"d",max.diff, sep=".")
+    return(myx)
+    
+  }
+}
+
+n <- length(myVariable)
+trend <- 1:n
+breaks <- 2
+model <- "crash"
 myVariable.diff <- diffmatrix(myVariable, max.diff = 1, max.lag = 1)
 # Run the test once to determine the values for the break dates
 # Definition of all the options needed
@@ -275,7 +427,7 @@ myZcoef <- na.omit(coef(lm(myVariable.diff ~ Z.diff)))
 
 # delta_1 from equation (7) of Chou 2007, which is needed for the manual calculation of S.tilde according to Chou 2007
 # equivalent results to the calculation with the cumulated sum
-delta_1 <- myVariable[1] - (Z[1,] %*% myZcoef)
+delta_1 <- as.numeric(myVariable[1] - (Z[1,] %*% myZcoef))
 
 #Result is equivalent to the original S.tilde
 myS.tilde <- myVariable - delta_1 - Z %*% myZcoef
@@ -293,7 +445,7 @@ boot.y <- foreach(i=1:n.sim, .combine = rbind, .packages = 'foreach') %dopar%{
   y.star <- delta_1 + as.vector((Z %*% myZcoef)) + sample(myS.tilde, size = length(myS.tilde), replace = TRUE)
   #y.star.matrix[,i] <- y.star
   #plotl(y.star)
-  test.stat <- ur.ls.bootstrap(y=y.star , model = model, breaks = breaks , method = "GTOS",pn = 0.1, critval = "bootstrap", print.results = "silent")
+  test.stat <- ur.ls.bootstrap(y=y.star , model = model, breaks = breaks, lags = 13, method = "Fixed",pn = 0.1, critval = "bootstrap", print.results = "silent")
   if(breaks == 2){
     return(list(c(unlist(test.stat[[1]]$`t-stat`)), c(unlist(test.stat[[1]]$`First break`)) ,c(unlist(test.stat[[1]]$`Second break`)),  c(as.numeric(test.stat[[1]]$Runtime, units = "mins"))))
   } else if (breaks == 1){
@@ -308,157 +460,8 @@ if(breaks == 2){
   colnames(boot.y) <- c("t-stat","First break",  "Runtime")
 }
 
-
-MPD_BOOTS_RESULTS <- test.results.y
-MPD_BOOTS_INTERVALS <- boot.y
-#Calculate the critical values, based on the distribution of the test statistic
-hist(unlist(MPD_BOOTS_INTERVALS[,"t-stat"]), freq = FALSE)
-quantile(unlist(MPD_BOOTS_INTERVALS[,"t-stat"]), c(.01, .05, .1, .25, .5, .75, .95, .99))
-quantile(unlist(MPD_BOOTS_INTERVALS[,"First break"]), c(.01, .05, .1, .25, .5, .75, .95))
-quantile(unlist(MPD_BOOTS_INTERVALS[,"Second break"]), c(.01, .05, .1, .25, .5, .75, .95))
-
-hist(unlist(MPD_BOOTS_INTERVALS[,"Second break"]), freq = F)
-hist(unlist(MPD_BOOTS_INTERVALS[,"First break"]), freq = F)
-
-MPD_BOOTS_RESULTS <- test.results.y
-MPD_BOOTS_INTERVALS <- boot.y
-
-
-
-
-
-
-#######################################
-# MPMs -----------------------------------
-######################################
-
-cl <- makeCluster(max(1, detectCores() - 2))
-
-registerDoSNOW(cl)
-#Define variable y, which is the variable to be analysed
-
-myVariable <- na.omit(as.numeric(MPM_MAFSZ))
-
-lagmatrix <- function(x,max.lag){
-  embed(c(rep(NA,max.lag),x),max.lag+1)
-}
-#Add diffmatrix function
-diffmatrix <- function(x,max.diff = 1,max.lag = 1){
-  #Add if condition to make it possible to differentiate between matrix and vector                  
-  if(is.vector(x) == TRUE ){
-    myx <- embed(c(rep(NA,max.lag),diff(x,max.lag,max.diff)),max.diff)
-    colnames(myx) <- paste("v1.d",max.diff, sep=".")
-    return(myx)
-  }
-  
-  else if(is.matrix(x) == TRUE){
-    myx <- rbind(matrix(rep(NA,max.lag), ncol = ncol(x)), matrix(diff(x,max.lag,max.diff), ncol = ncol(x)))
-    mycolnames <- colnames(x)
-    colnames(myx) <- paste(mycolnames,"d",max.diff, sep=".")
-    return(myx)
-  }
-  #if matrix the result should be 0, if only a vector it should be 1
-  else if(as.integer(is.null(ncol(x))) == 0 ){
-    myx <- rbind(matrix(rep(NA,max.lag), ncol = ncol(x)), matrix(diff(x,max.lag,max.diff), ncol = ncol(x)))
-    mycolnames <- colnames(x)
-    colnames(myx) <- paste(mycolnames,"d",max.diff, sep=".")
-    return(myx)
-    
-  }
-}
-
-n <- length(myVariable)
-trend <- 1:n
-breaks <- 2
-model <- "crash"
-myVariable.diff <- diffmatrix(myVariable, max.diff = 1, max.lag = 1)
-# Run the test once to determine the values for the break dates
-# Definition of all the options needed
-
-test.results.y <-  ur.ls.bootstrap(y=myVariable , model = model, breaks = breaks, lags = 5, method = "Fixed",pn = 0.1, critval = "bootstrap", print.results = "print")
-
-
-# Define variable Z, which is either Dt or DTt, depending on the model used
-# Create Dt and DTt, depending on the breaks, found in the initial application of the LS test
-
-#Define break dates
-myBreak1 <- test.results.y[[1]]$`First break`
-myBreak2 <- test.results.y[[1]]$`Second break`
-
-
-Dt1 <-  as.matrix(cbind(trend, trend >= (myBreak1 + 1)))
-
-#       Dummy with break in intercept and in trend
-
-DTt1 <- as.matrix(cbind(Dt1, c(rep(0, myBreak1), 1:(n - myBreak1))))
-colnames(Dt1) <- c("Trend","D")
-colnames(DTt1) <- c("Trend","D","DTt")
-Dt <- cbind(Dt1)
-DTt <- cbind(DTt1)
-
-
-Dt2 <-  as.matrix(trend >= (myBreak2 + 1))
-DTt2 <- as.matrix(cbind(Dt2, c(rep(0, myBreak2), 1:(n - myBreak2))))
-colnames(Dt2) <- c("D2")
-colnames(DTt2) <- c("D2","DTt2")
-#print(paste("Break2: ",myBreak2, sep = ""))
-
-#Combine all Dummies into one big matrix to make it easier to include in the regressions
-if(breaks == 1)
-{
-  Dt <- cbind(Dt1)
-  DTt <- cbind(DTt1)
-} else if(breaks == 2){
-  
-  Dt <- cbind(Dt1, Dt2)
-  DTt <- cbind(DTt1, DTt2)
-}
-
-if(model == "crash"){
-  Z <- Dt
-} else if(model == "break"){
-  
-  Z <- DTt
-}
-
-Z.diff <- diffmatrix(Z, max.diff = 1, max.lag = 1)
-#Matrix of coefficients, which omits the NA values
-myZcoef <- na.omit(coef(lm(myVariable.diff ~ Z.diff)))
-
-# delta_1 from equation (7) of Chou 2007, which is needed for the manual calculation of S.tilde according to Chou 2007
-# equivalent results to the calculation with the cumulated sum
-delta_1 <- myVariable[1] - (Z[1,] %*% myZcoef)
-
-#Result is equivalent to the original S.tilde
-myS.tilde <- myVariable - delta_1 - Z %*% myZcoef
-if(model == "crash"){
-  y.star <- delta_1 + as.vector((Dt %*% myZcoef)) + sample(myS.tilde, size = length(myS.tilde), replace = TRUE)
-} else if(model == "break"){
-  y.star <- delta_1 + as.vector((DTt %*% myZcoef)) + sample(myS.tilde, size = length(myS.tilde), replace = TRUE)
-  
-}
-
-
-#Loop for the actual boot strapping procedure
-
-boot.y <- foreach(i=1:n.sim, .combine = rbind, .packages = 'foreach') %dopar%{
-  y.star <- delta_1 + as.vector((Z %*% myZcoef)) + sample(myS.tilde, size = length(myS.tilde), replace = TRUE)
-  #y.star.matrix[,i] <- y.star
-  #plotl(y.star)
-  test.stat <- ur.ls.bootstrap(y=y.star , model = model, breaks = breaks, lags = 5, method = "Fixed",pn = 0.1, critval = "bootstrap", print.results = "silent")
-  if(breaks == 2){
-    return(list(c(unlist(test.stat[[1]]$`t-stat`)), c(unlist(test.stat[[1]]$`First break`)) ,c(unlist(test.stat[[1]]$`Second break`)),  c(as.numeric(test.stat[[1]]$Runtime, units = "mins"))))
-  } else if (breaks == 1){
-    return(c(c(unlist(test.stat[[1]]$`t-stat`)), c(unlist(test.stat[[1]]$`First break`)) , c(as.numeric(test.stat[[1]]$Runtime, units = "mins"))))
-  }
-}
-
-
-if(breaks == 2){
-  colnames(boot.y) <- c("t-stat","First break", "Second break", "Runtime")
-} else if (breaks == 1){
-  colnames(boot.y) <- c("t-stat","First break",  "Runtime")
-}
+MPM_BOOTS_RESULTS_crash <- test.results.y
+MPM_BOOTS_INTERVALS_crash <- boot.y
 
 
 #Calculate the critical values, based on the distribution of the test statistic
@@ -469,6 +472,3 @@ quantile(unlist(MPM_BOOTS_INTERVALS[,"Second break"]), c(.01, 0.25, .05, .1, .25
 
 hist(unlist(MPM_BOOTS_INTERVALS[,"Second break"]), freq = F)
 hist(unlist(MPM_BOOTS_INTERVALS[,"First break"]), freq = F)
-
-MPM_BOOTS_RESULTS <- test.results.y
-MPM_BOOTS_INTERVALS <- boot.y
